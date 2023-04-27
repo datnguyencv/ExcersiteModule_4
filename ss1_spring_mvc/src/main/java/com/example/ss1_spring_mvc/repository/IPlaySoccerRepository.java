a52c3cf3fef39761e4798b779b51fdf77647a921
tnq112 <79589849+tnq112@users.noreply.github.com>
2023-04-27 16:12:34 +0700

    update_ss1

--- ss1_spring_mvc/src/main/java/com/example/ss1_spring_mvc/repository/IPlaySoccerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.ss1_spring_mvc.repository;
-
-import com.example.ss1_spring_mvc.model.SoccerPlayer;
-
-import java.util.List;
-
-public interface IPlaySoccerRepository {
-    List<SoccerPlayer> findAll();
-}
--- ss1_spring_mvc/src/main/java/com/example/ss1_spring_mvc/service/IPlaySoccerService.java
+++
@@ -1,9 +0,0 @@
-package com.example.ss1_spring_mvc.service;
-
-import com.example.ss1_spring_mvc.model.SoccerPlayer;
-
-import java.util.List;
-
-public interface IPlaySoccerService {
-    List<SoccerPlayer> findAll();
-}
